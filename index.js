79ef171fec270cf32a2482371f6f2e7629fc79ca
inherited-architect-4472 <elaine.onb@gmail.com>
2018-11-11 01:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function setCart(c) {
 function addToCart(item) {
   var obj = {itemName: item, itemPrice: Math.floor(Math.random() * 50)};
   cart.push(obj);
-  return  `${item} has been added to your cart`;
+  return `${item} has been added to your cart`;
 }
 
 function viewCart() {
@@ -44,10 +44,9 @@ function viewCart() {
     if(cart.length === 0){
       return `Your shopping cart is empty.`;
     } else if (cart.length === 2) {
-      return `In your cart, you have ${cart[0]}
+      return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice},and ${cart[1].itemName} at ${cart[1].itemPrice}`;
     }
-    
-      for(var i = 0; i < cart.length; i++){
+    for(var i = 0; i < cart.length; i++){
   
 }
 
